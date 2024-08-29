3e7a1459a81756e9bdff96fce337b41e2a9c7dc4
zuka <zurabi.chachava.1@btu.edu.ge>
2024-08-29 23:35:01 +0400

    add tasks with result and error

---
+++ task/multiple/tasks-no-error/tasks_no_error.go
@@ -0,0 +1,41 @@
+﻿package tasks_no_error
+
+import "sync"
+
+type Tasks struct {
+	tasks []func()
+}
+
+func NewTasks() *Tasks {
+	return &Tasks{}
+}
+
+func (tasks *Tasks) Add(task func()) {
+	tasks.tasks = append(tasks.tasks, task)
+}
+
+func (tasks *Tasks) Run() *TasksResult {
+	wg := sync.WaitGroup{}
+
+	for _, task := range tasks.tasks {
+		wg.Add(1)
+
+		go func(wg *sync.WaitGroup) {
+			defer wg.Done()
+			task()
+		}(&wg)
+	}
+
+	clear(tasks.tasks)
+	return &TasksResult{
+		wg: &wg,
+	}
+}
+
+type TasksResult struct {
+	wg *sync.WaitGroup
+}
+
+func (tasksResult *TasksResult) Wait() {
+	tasksResult.wg.Wait()
+}
---
+++ task/multiple/tasks-result-error/tasks_result_error.go
@@ -0,0 +1,60 @@
+﻿package tasks_result_error
+
+import "sync"
+
+type TasksResultError[T any, TErr error] struct {
+	tasks []func() (T, TErr)
+}
+
+func NewTasksResultError[T any, TErr error]() *TasksResultError[T, TErr] {
+	return &TasksResultError[T, TErr]{}
+}
+
+func (tasks *TasksResultError[T, TErr]) Add(task func() (T, TErr)) *TasksResultError[T, TErr] {
+	tasks.tasks = append(tasks.tasks, task)
+	return tasks
+}
+
+func (tasks *TasksResultError[T, TErr]) Run() *TasksResult[T, TErr] {
+	wg := sync.WaitGroup{}
+	channel := make(chan tupleWrapper[T, TErr], len(tasks.tasks))
+
+	for index, task := range tasks.tasks {
+		wg.Add(1)
+		go func(wg *sync.WaitGroup, channel chan tupleWrapper[T, TErr], task func() (T, TErr)) {
+			defer wg.Done()
+			result, err := task()
+			channel <- tupleWrapper[T, TErr]{index, result, err}
+		}(&wg, channel, task)
+	}
+
+	return &TasksResult[T, TErr]{size: len(tasks.tasks), wg: &wg, channel: channel}
+}
+
+type TasksResult[T any, TErr error] struct {
+	size    int
+	wg      *sync.WaitGroup
+	channel chan tupleWrapper[T, TErr]
+}
+
+func (tasks *TasksResult[T, TErr]) Wait() []ResultTuple[T, TErr] {
+	tasks.wg.Wait()
+	data := make([]ResultTuple[T, TErr], tasks.size)
+
+	for result := range tasks.channel {
+		data = append(data, ResultTuple[T, TErr]{Value: result.value, Err: result.err})
+	}
+
+	return data
+}
+
+type tupleWrapper[T any, TErr error] struct {
+	index int
+	value T
+	err   TErr
+}
+
+type ResultTuple[T any, TErr error] struct {
+	Value T
+	Err   TErr
+}
