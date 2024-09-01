0566b27dbbc542f29eb464a0933c023616685847
zuka <zurabi.chachava.1@btu.edu.ge>
2024-09-01 14:39:46 +0400

    added test cases

---
+++ task_test/multiple_test/tasks_no_result_test.go
@@ -0,0 +1,25 @@
+﻿package multiple
+
+import (
+	tasks "github.com/zukaChachava/task/task/multiple/tasks-no-result"
+	"testing"
+	"time"
+)
+
+func Test_RunWithNoResult(t *testing.T) {
+	tasksContainer := tasks.NewTasks()
+
+	for i := 0; i < 10; i++ {
+		tasksContainer.Add(func() {
+			time.Sleep(1 * time.Second)
+		})
+	}
+
+	currentTime := time.Now()
+	tasksContainer.Run().Wait()
+	duration := time.Since(currentTime)
+
+	if duration > time.Second*2 {
+		t.Fatal("Not running concurrently")
+	}
+}
---
+++ task_test/multiple_test/tasks_result_error_test.go
@@ -0,0 +1,34 @@
+﻿package multiple
+
+import (
+	tasks "github.com/zukaChachava/task/task/multiple/tasks-result-error"
+	"testing"
+	"time"
+)
+
+func Test_RunWithResultError(t *testing.T) {
+	tasksContainer := tasks.NewTasks[int, error]()
+
+	for i := 0; i < 10; i++ {
+		tasksContainer.Add(func() (int, error) {
+			time.Sleep(1 * time.Second)
+			return i, nil
+		})
+	}
+
+	currentTime := time.Now()
+	results := tasksContainer.Run().Wait()
+	duration := time.Since(currentTime)
+
+	num := 0
+	for result := range results {
+		if num != result {
+			t.Fatal("Invalid result")
+		}
+		num++
+	}
+
+	if duration > time.Second*2 {
+		t.Fatal("Not running concurrently")
+	}
+}
